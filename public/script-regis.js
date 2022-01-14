96103ce5d5c696ec9cfa3b4147c72ead7b968354
ngocanhpcy <93309537+ngocanhpcy@users.noreply.github.com>
2022-01-14 18:30:42 +0700

    final project

---
+++ public/script-regis.js
@@ -0,0 +1,98 @@
+﻿const prevBtns = document.querySelectorAll(".btn-prev");
+const nextBtns = document.querySelectorAll(".btn-next");
+const progress = document.getElementById("progress");
+const formSteps = document.querySelectorAll(".form-step");
+const progressSteps = document.querySelectorAll(".progress-step");
+//var email = form.getElementById("email").value;
+//var password = form.getElementById("password").value;
+//var confirmPassword = form.getElementById("confirmPassword").value;
+let form = document.querySelector('form');
+form.onsubmit = () => { return false; }
+
+
+let formStepsNum = 0;
+
+
+nextBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        validate();
+     //   checkEmail();
+     //   checkPass();
+    });
+});
+
+prevBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        formStepsNum--;
+        updateFormSteps();
+        updateProgressbar();
+    });
+});
+//function checkEmail() {
+//    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+//    if (!email.value.match(pattern)) {
+//        input.nextElementSibling.textContent = "Enter a valid email address";
+//        //enter a valid email address until email valid, else show email can't be blank
+//    } else if (email == "") {
+//        input.nextElementSibling.textContent = "This field is empty";
+//    } else {
+//        input.nextElementSibling.textContent = "";
+//    }
+//}
+//function checkPass() {
+//    if (password != confirmPassword) {
+//        input.nextElementSibling.textContent = "Passwords do not match";
+//        return false;
+//    }
+//    return true;
+//}
+function validate() {
+    const activePage = document.querySelector('.form-step-active');
+    const inputgroup = activePage.getElementsByClassName('input-group');
+    let inputFirst = inputgroup[0].children[1];
+    let inputLast = inputgroup[1].children[1];
+
+    if (inputFirst.value != "" && inputLast.value != "") {
+        formStepsNum++;
+        updateFormSteps();
+        updateProgressbar();
+        form.onsubmit = () => { return true; }
+    }
+    if (inputFirst.value == "") {
+        hide(inputFirst);
+    }
+    if (inputLast.value == "") {
+        hide(inputLast);
+    }
+
+
+}
+function hide(input) {
+    input.nextElementSibling.textContent = "This field is empty";
+    setTimeout(() => {
+        input.nextElementSibling.textContent = "";
+    }, 2000);
+}
+function updateFormSteps() {
+    formSteps.forEach((formStep) => {
+        formStep.classList.contains("form-step-active") &&
+            formStep.classList.remove("form-step-active");
+    });
+
+    formSteps[formStepsNum].classList.add("form-step-active");
+}
+
+function updateProgressbar() {
+    progressSteps.forEach((progressStep, idx) => {
+        if (idx < formStepsNum + 1) {
+            progressStep.classList.add("progress-step-active");
+        } else {
+            progressStep.classList.remove("progress-step-active");
+        }
+    });
+
+    const progressActive = document.querySelectorAll(".progress-step-active");
+
+    progress.style.width =
+        ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
+}
---
+++ public/script.js
@@ -0,0 +1,108 @@
+﻿let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+let eField = loginForm.querySelector('.email');
+let eInput = eField.querySelector('input');
+let pField = loginForm.querySelector('.password');
+let pInput = pField.querySelector('input');
+
+loginForm.onsubmit = (e) => {
+    e.preventDefault(); //prevent form from submittng
+    if (eInput.value == "") { //if email is empty
+        eField.classList.add("shake", "error");
+    } else {
+        checkEmail(); //calling checkEmail function
+    }
+    if (pInput.value == "") { //if password is empty
+        pField.classList.add("shake", "error");
+    }
+
+    setTimeout(() => { //remove shake class after 500ms
+        eField.classList.remove("shake");
+        pField.classList.remove("shake");
+    }, 500);
+
+    //work on input keyup
+    eInput.onkeyup = () => {
+        checkEmail(); //calling checkemail function
+    }
+
+    function checkEmail() {
+        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //pattern to validate email
+        if (!eInput.value.match(pattern)) { //if pattern not matched with user entered value
+            eField.classList.add("error");
+            let errorTxt = eField.querySelector('.error-txt');
+            //enter a valid email address until email valid, else show email can't be blank
+            (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address" : errorTxt.innerText = "Email can't be blank";
+        } else {
+            eField.classList.remove("error");
+        }
+    }
+
+    pInput.onkeyup = () => {
+        if (pInput.value == "") { //if pattern not matched with user entered value
+            pField.classList.add("error");
+        } else {
+            pField.classList.remove("error");
+        }
+    }
+    //if error class not contain in email and pass then user can enter
+    if (!eField.classList.contains("error") && !pField.classList.contains("error")) {
+        window.location.href = "home.html";
+        console.log("Form submitted");
+    }
+}
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+
+}
+
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let slides = document.querySelectorAll('.home .slides-container .slide');
+let index = 0;
+
+function next() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
